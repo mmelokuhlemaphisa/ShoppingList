a96194adf1a1fb6e9f0d98cc441477ea2352f0ea
Melokuhle <melokuhlemaphisa99@gmail.com>
2026-01-12 14:37:44 +0200

    feat:Removed index on top of my app in layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
+// app/_layout.tsx
 import { Stack } from "expo-router";
+import { Provider } from "react-redux";
+import { store } from "../src/redux/store";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Provider store={store}>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+            title: "Shopping List",
+          }}
+        />
+      </Stack>
+    </Provider>
+  );
 }
